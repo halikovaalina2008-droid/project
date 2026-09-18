eb93870e196537fd65b5af44dd467cad44f6eb8b
alinaaa <halikovaalina2008@gmail.com>
2026-09-18 11:46:01 +0500

    Add files via upload

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
-package main 
-import "fmt"
+package main
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello word")
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ { 
+	s += sep + os.Args[i] 
+	sep = " "
+	}
+	fmt.Println(s)
 }
